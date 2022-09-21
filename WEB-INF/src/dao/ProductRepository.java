a912140356cea933602572cd4794ff98a1201018
이태원 <dlxodnjs1005@gmail.com>
2022-09-21 08:19:50 +0000

    서블릿 프로젝트 기본 파일

--- WEB-INF/src/dao/ProductRepository.java
+++ WEB-INF/src/dao/ProductRepository.java
@@ -7,11 +7,11 @@ public class ProductRepository {
     
     public ProductRepository() {
         Product phone = new Product("p1234", "iphone 6s", 800000);
-        Phone.setDescription("4.7-inch, 1334X750 Renina HD display, 8-megapixel iSight Camera");
-        Phone.setCategory("Smart Phone");
-        Phone.setManufacturer("Apple");
+        phone.setDescription("4.7-inch, 1334X750 Renina HD display, 8-megapixel iSight Camera");
+        phone.setCategory("Smart Phone");
+        phone.setManufacturer("Apple");
         phone.setUnitsInStock(1000);
-        Phone.setCondition("NEW");
+        phone.setCondition("NEW");
         
         listOfProducts.add(phone);
         
